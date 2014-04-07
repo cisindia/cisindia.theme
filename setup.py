870abd4b97d57ca9199825d06648b06197bf5c84
Khairul Anwar (kulll) <anwarbaik88@gmail.com>
2014-04-07 12:36:19 +0800

    update dependency

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(name='cisindia.theme',
           'collective.portlet.collectionmultiview',
           'fourdigits.portlet.twitter',
           'cisindia.multiviews',
+          'collective.contentleadimage',
           # -*- Extra requirements: -*-
       ],
       extras_require={
